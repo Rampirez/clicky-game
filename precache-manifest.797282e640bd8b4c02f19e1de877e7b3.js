f10ee9f5e494c1ea734336dd7e5bdbce1e7973f3
Mathew Ramirez <Mathew.J.Ramirez@gmail.com>
2020-02-24 23:23:43 -0600

    Updates

---
+++ precache-manifest.797282e640bd8b4c02f19e1de877e7b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef12046edeee7d67f3350126b82f1379",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "301ea3aefd5693418b01",
+    "url": "/clicky-game/static/css/main.6e4a6ab4.chunk.css"
+  },
+  {
+    "revision": "646a75c58be7259d17ba",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301ea3aefd5693418b01",
+    "url": "/clicky-game/static/js/main.1bf17d8c.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
